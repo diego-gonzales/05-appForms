cf5b279d85617545bfe94ea3690760085ab086e4
Diego Gonzales <dgonzalesj14@unc.edu.pe>
2021-03-18 18:20:08 -0500

    Terminada seccion donde se trabaja con formularios reactivos

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -8,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicsComponent implements OnInit {
 
-  constructor() { }
+  // Esto es equivalente a lo de abajo
+  // myFormcito: FormGroup = new FormGroup({
+  //   product_name:  new FormControl('RTX 2080Ti'),
+  //   product_price: new FormControl(1500),
+  //   product_stock: new FormControl(5),
+  // });
 
-  ngOnInit(): void {
+
+  // el servicio form builder se usa cuando tenemos forms demasiado complejos para tener un codigo mas limpio, pero lo de arriba es valido
+  myFormcito: FormGroup = this.formBuilder.group({
+    product_name: [ , [Validators.required, Validators.minLength(3)] ],
+    product_price: [ , [Validators.required, Validators.min(0)] ],
+    product_stock: [ , [Validators.required, Validators.min(0)] ]
+  });
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    // si establecemos valores por defecto con setValue() entonces todos los campos tienen que ir obligatoriamente
+    // this.myFormcito.setValue({
+    //   product_name: 'RTX 4080 Ti',
+    //   product_price: 1200,
+    //   product_stock: 10
+    // });
+    // si lo hacemos con reset() no es obligatorio que vayan todos los campos
+    this.myFormcito.reset({
+      product_name: 'RTX 4080 Ti',
+      product_price: 1200,
+      // product_stock: 10
+    });
   }
+
+  // Methods
+
+  fieldIsNotValid( controlName: string ) {
+    return this.myFormcito.controls[controlName].errors && this.myFormcito.controls[controlName].touched;
+  };
+
+
+  save() {
+
+    if( this.myFormcito.invalid ) {
+      this.myFormcito.markAllAsTouched();
+      return;
+    };
+
+    console.log(this.myFormcito.value);
+    this.myFormcito.reset();
+  };
+
 
 }
--- src/app/reactive/dinamics/dinamics.component.ts
+++ src/app/reactive/dinamics/dinamics.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
+
+interface Person {
+  name: string;
+  games: Game[];
+};
+
+interface Game {
+  id: number;
+  title: string;
+};
 
 @Component({
   selector: 'app-dinamics',
@@ -6,11 +18,72 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
 
-  constructor() { }
+  // Variables
 
-  ngOnInit(): void {
-  }
+  myFormcito: FormGroup = this.formBuilder.group({
+
+    name_user: [ , [Validators.required, Validators.minLength(3)] ],
+    list_games: this.formBuilder.array( [
+      ['Pokemon Go', Validators.required], // esto nos son arreglos
+      ['Team Fortress 2', Validators.required]
+    ], Validators.required )
+
+  });
+
+  // newControlGame: FormControl = new FormControl('abc', Validators.required); // este es igual a lo de abajo
+  newControlGame: FormControl = this.formBuilder.control('', [Validators.required, Validators.minLength(3)]);
+
+  get gamesArr() {
+    return this.myFormcito.get('list_games') as FormArray; 
+    // return this.myFormcito.controls.list_games.controls; // Esto es lo mismo que lo de arriba
+  };
+
+  // Constructor
+  constructor( private formBuilder: FormBuilder) { }
+
+
+  // Methods
+  fieldIsNotValid( controlName: string ) {
+    return this.myFormcito.controls[controlName].errors && this.myFormcito.controls[controlName].touched;
+  };
+
+  // valido por aparte este campo solo necesita que haya errores y no necesariamente tiene que ser tocado
+  fieldListGamesInvalid() {
+    return this.myFormcito.controls.list_games.errors;
+  };
+
+  // esta validacion creo que esta demas porque a veces el usuario puede escribir algo y luego borrarlo y le va a aparecer el error
+  // cosa que no deberia ser asi, y lo mejor seria poner este newControlGame como que no es required, pero lo dejamos asi
+  fieldGameInvalid() {
+    return this.newControlGame.errors && !this.newControlGame.pristine;
+  };
+
+  addGame() {
+    if ( this.newControlGame.invalid ) { return };
+    
+    // (this.myFormcito.get('list_games') as FormArray).push(this.newControlGame);
+    // podria poner el control nuevo directamente pero no es lo mas recomendable por eso usamos el formBuilder
+    this.gamesArr.push( this.formBuilder.control(this.newControlGame.value, Validators.required) );
+
+    // pero si reseteo ese control nuevo ya no habria problema en agregarlo directamente, pero lo mejor es con el formBuilder
+    this.newControlGame.reset();
+  };
+
+
+  deleteGame( indexGame: number ) {
+    this.gamesArr.removeAt(indexGame);
+  };
+
+
+  save() {
+    if( this.myFormcito.invalid ) {
+      this.myFormcito.markAllAsTouched();
+      return;
+    };
+    
+    console.log(this.myFormcito.value);
+  };
 
 }
